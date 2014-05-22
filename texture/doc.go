6c567fabf0c383939b574ffca6e909174a89bec7
mewmew <rnd0x00@gmail.com>
2014-05-22 23:27:14 +0200

    texture: Add skeleton of the API.

---
+++ texture/doc.go
@@ -0,0 +1,5 @@
+// Package texture handles hardware accelerated image drawing. It uses a small
+// subset of the features provided by the GLFW library version 3.0 [1].
+//
+// [1]: http://www.glfw.org/
+package texture
